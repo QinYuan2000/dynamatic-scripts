9c610d151a00f4908e559b5efb9220cd7ec6109a
Jiahui Xu <jxu@ethz.ch>
2024-03-19 15:23:23 +0100

    update CP option in the synthesis script

--- run.sh
+++ run.sh
@@ -1,14 +1,16 @@
 set-dynamatic-path  ./dynamatic
 
+set-clock-period 5.2
+
 set-src             ./dynamatic/integration-test/fir/fir.c
 # set-src             ./dynamatic/integration-test/iir/iir.c
 # set-src             ./dynamatic/integration-test/gemver/gemver.c
 
 compile
 
-# write-hdl
+write-hdl
 
-# simulate
+simulate
 
 exit
 
